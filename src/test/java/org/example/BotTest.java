0aa6c34d2d0df69aa6cdc9267b6787a1304a4cae
ALEXEY-PEREVOSHCHIKOV <114176011+ALEXEY-PEREVOSHCHIKOV@users.noreply.github.com>
2023-10-16 18:24:59 +0500

    BotTest.java

--- src/test/java/org/example/BotTest.java
+++ src/test/java/org/example/BotTest.java
@@ -1,31 +1,57 @@
 package org.example;
+import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
 public class BotTest {
     @Test
     public void parseTest() {
-        // Проверка для команды /start
-        assertEquals("Приветствую, это литературный бот. Жми /get, чтобы получить случайную цитату. Жми /genre, чтобы перейти в раздел жанры книг", Bot.parseMessage("/start"));
+        Bot bot = new Bot();
 
-        // Проверка для команды /help
-        assertEquals("Приветствую, это литературный бот. Жми /get, чтобы получить случайную цитату. Жми /genre, чтобы перейти в раздел жанры книг", Bot.parseMessage("/help"));
+        /**
+         Проверка для команды /start
+         */
+        String response = bot.parseMessage("/start");
+        Assert.assertEquals("Приветствую, это литературный бот. Жми /get, чтобы получить случайную цитату. Жми /genre, чтобы перейти в раздел жанры книг", response);
 
-        // Проверка для команды /genre
-        assertEquals("Здравствуйте, добро пожаловать в бот рекомендации книг! Выберите жанр:", Bot.parseMessage("/genre"));
+        /**
+         Проверка для команды /help
+         */
+        response = bot.parseMessage("/help");
+        Assert.assertEquals("Приветствую, это литературный бот. Жми /get, чтобы получить случайную цитату. Жми /genre, чтобы перейти в раздел жанры книг", response);
 
-        // Проверка для жанра "Научная фантастика"
-        assertEquals("Прочитайте 'Автостопом по галактике', 'Время жить и время умирать' или 'Война миров'", Bot.parseMessage("Научная фантастика"));
+        /**
+         Проверка для команды /genre
+         */
+        response = bot.parseMessage("/genre");
+        Assert.assertEquals("Здравствуйте, добро пожаловать в бот рекомендации книг! Выберите жанр:", response);
 
-        // Проверка для жанра "Фэнтези"
-        assertEquals("Прочитайте 'Хоббит', 'Игра престолов' или 'Гарри Поттер'", Bot.parseMessage("Фэнтези"));
+        /**
+         Проверка для жанра "Научная фантастика"
+         */
+        response = bot.parseMessage("Научная фантастика");
+        Assert.assertEquals("Прочитайте 'Автостопом по галактике', 'Время жить и время умирать' или 'Война миров'", response);
 
-        // Проверка для жанра "Романтика"
-        assertEquals("Прочитайте 'Великий Гетсби', 'Триумфальная арка' или 'Поющие в терновнике'", Bot.parseMessage("Романтика"));
+        /**
+         Проверка для жанра "Фэнтези"
+         */
+        response = bot.parseMessage("Фэнтези");
+        Assert.assertEquals("Прочитайте 'Хоббит', 'Игра престолов' или 'Гарри Поттер'", response);
 
-        // Проверка для жанра "Детектив"
-        assertEquals("Прочитайте 'Убийство в восточном экспрессе', 'Снеговик' или 'Собака Баскервилей'", Bot.parseMessage("Детектив"));
+        /**
+         Проверка для жанра "Романтика"
+         */
+        response = bot.parseMessage("Романтика");
+        Assert.assertEquals("Прочитайте 'Великий Гетсби', 'Триумфальная арка' или 'Поющие в терновнике'", response);
 
-        // Проверка для произвольного сообщения
-        assertEquals("Привет", Bot.parseMessage("Привет"));
+        /**
+         Проверка для жанра "Детектив"
+         */
+        response = bot.parseMessage("Детектив");
+        Assert.assertEquals("Прочитайте 'Убийство в восточном экспрессе', 'Снеговик' или 'Собака Баскервилей'", response);
+
+        /**
+         Проверка для произвольного сообщения
+         */
+        response = bot.parseMessage("Привет");
+        Assert.assertEquals("Привет", response);
     }
 }
