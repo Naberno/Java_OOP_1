bdaefadeb72197d8b2323da3cde82f2d3f49b522
ALEXEY-PEREVOSHCHIKOV <114176011+ALEXEY-PEREVOSHCHIKOV@users.noreply.github.com>
2023-10-16 18:23:44 +0500

    Bot.java

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -4,34 +4,32 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
-    final private String BOT_TOKEN = "***";
-    final private String BOT_NAME = "groobee";
-    static Storage storage;
-
+    final private String BOT_TOKEN = ****;
+    final private String BOT_NAME = ****;
+    Storage storage;
     Bot() {
         storage = new Storage();
     }
-
     @Override
     public String getBotUsername() {
         return BOT_NAME;
     }
-
     @Override
     public String getBotToken() {
         return BOT_TOKEN;
     }
 
     @Override
+    /**
+     Получение и Отправка сообщения в чат пользователю
+     */
     public void onUpdateReceived(Update update) {
         try {
             if (update.hasMessage() && update.getMessage().hasText()) {
@@ -43,12 +41,10 @@ public class Bot extends TelegramLongPollingBot {
                 String response = parseMessage(inMess.getText());
                 //Создаем объект класса SendMessage - наш будущий ответ пользователю
                 SendMessage outMess = new SendMessage();
-
-                //Добавляем в наше сообщение id чата а также наш ответ
+                //Добавляем в наше сообщение id чата, а также наш ответ
                 outMess.setChatId(chatId);
                 outMess.setText(response);
                 outMess.setReplyMarkup(createKeyboard());
-
                 //Отправка в чат
                 execute(outMess);
             }
@@ -57,9 +53,11 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-    public static String parseMessage(String textMsg) {
+    /**
+     Обработчик сообщений пользователя
+     */
+    public String parseMessage(String textMsg) {
         String response;
-
         //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
         if (textMsg.equals("/start") || textMsg.equals("/help"))
             response = "Приветствую, это литературный бот. Жми /get, чтобы получить случайную цитату. Жми /genre, чтобы перейти в раздел жанры книг";
@@ -75,21 +73,22 @@ public class Bot extends TelegramLongPollingBot {
             response = "Прочитайте 'Великий Гетсби', 'Триумфальная арка' или 'Поющие в терновнике'";
         else if (textMsg.equals("Детектив"))
             response = "Прочитайте 'Убийство в восточном экспрессе', 'Снеговик' или 'Собака Баскервилей'";
-        else
-             response = textMsg;
-
+        else {
+            response = textMsg;
+        }
         return response;
     }
 
+    /**
+     Метод для создания клавиатуры в боте
+     */
     public ReplyKeyboardMarkup createKeyboard() {
         ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
         List<KeyboardRow> keyboard = new ArrayList<>();
-
         // Создание ряда клавиш
         KeyboardRow row1 = new KeyboardRow();
         row1.add("Детектив");
         row1.add("Романтика");
-
         KeyboardRow row2 = new KeyboardRow();
         row2.add("Фэнтези");
         row2.add("Научная фантастика");
