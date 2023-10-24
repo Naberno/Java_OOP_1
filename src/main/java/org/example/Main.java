22efe65e5969c8fb49f4ef956e9ee816bc0b0074
Naberno <burusov.vl@gmail.com>
2023-10-24 15:39:39 +0500

    Добавлены JavaDoc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,19 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
+    /**
+     * Главный метод, который запускает бота.
+     * Создает объект TelegramBotsApi, который используется для регистрации бота.
+     * Регистрирует новый экземпляр класса TelegramBot с помощью метода registerBot().
+     * Если при регистрации происходит ошибка, то выводит ее в консоль.
+     */
     public static void main(String[] args)
     {
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(new TelegramBot());
-        } catch (TelegramApiException e) {
+        }
+        catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
