716055b6ee813a150ebb18e402d5163698e4fce4
ALEXEY-PEREVOSHCHIKOV <114176011+ALEXEY-PEREVOSHCHIKOV@users.noreply.github.com>
2023-11-04 21:01:33 +0500

    Добавлены интерфейс для работы с классом MessageHandling, а также два режима для работы с игрой в загадки и режим по умолчанию
    Добавлены интерфейсы для работы с классом MessageHandling,  два режима handlePuzzleMode для работы с игрой в загадки и режим по умолчанию handleDefaultMode и описаны новые команды для пользователя в чате /gethint, /anotheriddle, /statistic, /restart, /getanswer, /stoppuzzle, /removebook и /playpuzzle

--- src/main/java/org/example/MessageHandling.java
+++ src/main/java/org/example/MessageHandling.java
@@ -1,20 +1,101 @@
 package org.example;
-
 import java.util.ArrayList;
+
+/**
+ * Интерфейс для обработки сообщений пользователя
+ */
+ interface MessageProcessor {
+
+    /**
+     * Обрабатывает текстовое сообщение пользователя и возвращает ответ.
+     *
+     * @param textMsg Текстовое сообщение пользователя.
+     * @param chatId  Идентификатор чата пользователя.
+     * @return Ответ на сообщение пользователя.
+     */
+    String parseMessage (String textMsg, long chatId);
+}
+
 
 /**
  * Класс для обработки сообщений пользователя
  */
-public class MessageHandling {
-    Storage storage;
-    public MessageHandling() {
-        storage = new Storage();
-    }
+public class MessageHandling implements MessageProcessor {
+    private Storage storage;
+    private PuzzleGame puzzleGame;
+    private boolean puzzleMode;
 
     /**
-     * Метод для обработки сообщений пользователя
+     * Конструктор класса MessageHandling. Инициализирует объекты Storage и PuzzleGame,
+     * а также устанавливает начальное значение режима головоломки как false.
      */
+    public MessageHandling() {
+        storage = new Storage();
+        puzzleGame = new PuzzleGame();
+        puzzleMode = false;
+    }
+
+
+    /**
+     * Метод для обработки входящего текстового сообщения от пользователя.
+     *
+     * @param textMsg Входящий текстовый запрос от пользователя.
+     * @param chatId  Идентификатор чата пользователя.
+     * @return Ответ на запрос пользователя в виде строки.
+     */
+
     public String parseMessage(String textMsg, long chatId) {
+        String response;
+
+        if (puzzleMode) {
+            response = handlePuzzleMode(textMsg, chatId);
+        } else {
+            response = handleDefaultMode(textMsg, chatId);
+        }
+
+        return response;
+    }
+
+
+    /**
+     * Обработчик сообщений в режиме головоломки.
+     *
+     * @param textMsg Входящий текстовый запрос от пользователя.
+     * @param chatId  Идентификатор чата пользователя.
+     * @return Ответ на запрос пользователя в режиме головоломки.
+     */
+    private String handlePuzzleMode(String textMsg, long chatId) {
+        String response;
+        if ((textMsg.equalsIgnoreCase("дай подсказку"))||(textMsg.equals("/gethint"))) {
+            response = puzzleGame.getHint();
+        } else if ((textMsg.equalsIgnoreCase("следующая загадка"))||(textMsg.equals("/anotheriddle"))) {
+            response = puzzleGame.getNextPuzzle(chatId);
+        } else if (textMsg.equals("/statistic")) {
+            response = puzzleGame.getStatistics(chatId);
+        } else if (textMsg.equals("/restart")) {
+            response = puzzleGame.restart(chatId);
+        } else if ((textMsg.equalsIgnoreCase("какой ответ"))||(textMsg.equals("/getanswer"))) {
+            response = puzzleGame.getAnswerAndNextPuzzle(chatId);
+            /* } else if ((textMsg.equalsIgnoreCase("покажи нерешённые загадки"))||(textMsg.equals("/showriddles"))) {
+            response = puzzleGame.getUnsolvedPuzzles(chatId); */
+        } else if (textMsg.equals("/stoppuzzle")) {
+            response = "Режим головоломки завершен.";
+            puzzleMode = false; // Выход из режима головоломки
+        } else {
+            response = puzzleGame.checkAnswer(chatId, textMsg);
+        }
+        return response;
+    }
+
+
+    /**
+     * Обработчик сообщений в режиме по умолчанию.
+     *
+     * @param textMsg Входящий текстовый запрос от пользователя.
+     * @param chatId  Идентификатор чата пользователя.
+     * @return Ответ на запрос пользователя в режиме по умолчанию.
+     */
+    private String handleDefaultMode(String textMsg, long chatId) {
         String response;
         // Сравниваем текст пользователя с командами, на основе этого формируем ответ
         if (textMsg.equals("/start") || textMsg.equals("/help")) {
@@ -32,6 +113,7 @@ public class MessageHandling {
         } else if (textMsg.equals("Детектив")) {
             response = "Прочитайте 'Убийство в восточном экспрессе', 'Снеговик' или 'Собака Баскервилей'";
 
+
         } else if (textMsg.startsWith("/addbook")) {
             // Получаем название книги, автора и год прочтения, введенные пользователем
             String[] parts = textMsg.substring(9).split("\n");
@@ -44,47 +126,73 @@ public class MessageHandling {
                     // Добавляем книгу в базу данных
                     storage.addReadBook(title, author, year, chatId);
                     response = "Книга '" + title + "' от автора " + author + " (год: " + year + ") успешно добавлена в список прочитанных!";
-                }
-                catch (NumberFormatException e) {
+                } catch (NumberFormatException e) {
                     response = "Некорректный формат года прочтения.";
                 }
-            }
-            else {
-                response = "Некорректный формат ввода. Используйте /addbook Название книги\nАвтор\nГод прочтения";
+            } else {
+                response = "Некорректный формат ввода. Используйте /haveread Название книги\nАвтор\nГод прочтения";
             }
 
+
         } else if (textMsg.equals("/clearread")) {
-        // Очищаем список прочитанных книг
-        storage.clearReadBooks(chatId);
-        response = "Список прочитанных книг очищен!";
+            // Очищаем список прочитанных книг
+            storage.clearReadBooks(chatId);
+            response = "Список прочитанных книг очищен!";
+
 
         } else if (textMsg.equals("/getread")) {
             // Получаем список прочитанных книг
             ArrayList<String> readBooks = storage.getReadBooks(chatId);
             response = "Прочитанные книги:\n" + String.join("\n", readBooks);
 
-        }else if (textMsg.startsWith("/getbyauthor")) {
-                String author = textMsg.substring(13); // Получаем имя автора из сообщения
-                ArrayList<String> booksByAuthor = storage.getBooksByAuthor(author, chatId);
-                if (!booksByAuthor.isEmpty()) {
-                    response = "Книги автора " + author + ":\n" + String.join("\n", booksByAuthor);
-                } else {
-                    response = "Нет прочитанных книг этого автора.";
-                }
 
-        }else if (textMsg.startsWith("/getbyyear")) {
+        } else if (textMsg.startsWith("/getbyauthor")) {
+            String author = textMsg.substring(13); // Получаем имя автора из сообщения
+            ArrayList<String> booksByAuthor = storage.getBooksByAuthor(author, chatId);
+            if (!booksByAuthor.isEmpty()) {
+                response = "Книги автора " + author + ":\n" + String.join("\n", booksByAuthor);
+            } else {
+                response = "Нет прочитанных книг этого автора.";
+            }
+
+
+        } else if (textMsg.startsWith("/getbyyear")) {
             int year = Integer.parseInt(textMsg.substring(11)); // Получаем год из сообщения
             ArrayList<String> getBooksByYear = storage.getBooksByYear(year, chatId);
             if (!getBooksByYear.isEmpty()) {
                 response = "Книги" + " " + year + " " + "года" + ":\n" + String.join("\n", getBooksByYear);
             } else {
-                response = "Нет прочитанных книг этого автора.";
+                response = "Нет прочитанных книг в этом году.";
             }
 
-            } else {
-            response = textMsg;
+
+        } else if (textMsg.startsWith("/removebook")) {
+        // Получаем название книги, автора и год прочтения, введенные пользователем
+        String[] parts = textMsg.substring(12).split("\n");
+        if (parts.length == 3) {
+            String title = parts[0].trim();
+            String author = parts[1].trim();
+            int year;
+            try {
+                year = Integer.parseInt(parts[2].trim());
+                // Удаляем книгу из базы данных
+                storage.RemoveReadBook(title, author, year, chatId);
+                response = "Книга '" + title + "' от автора " + author + " (год: " + year + ") успешно удалена из списка прочитанных!";
+            } catch (NumberFormatException e) {
+                response = "Некорректный формат года прочтения.";
+            }
+        } else {
+            response = "Некорректный формат ввода. Используйте /removebook Название книги\nавтор\nгод прочтения";
         }
 
+    } else if (textMsg.equals("/playpuzzle")) {
+            // Вход в режим головоломки
+            puzzleMode = true;
+            response = puzzleGame.startPuzzle(chatId);
+
+    }else {
+        response = textMsg;
+    }
         return response;
     }
 }
